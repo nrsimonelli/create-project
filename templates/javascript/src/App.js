da699d844a8bfa75c349f7ba58db4c8a4a54928f
nrsimonelli <nrsimonelli@gmail.com>
2021-06-08 21:12:52 -0500

    template update

---
+++ templates/javascript/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/main.css';
+
+const App = () => {
+  return (
+    <div className='root'>
+      <h1>hello friend</h1>
+    </div>
+  ); // end return
+};
+
+export default App;
