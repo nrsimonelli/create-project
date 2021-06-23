4c0b59198cd3f7f65fb57ba3c204d48bd423bdfa
nrsimonelli <nrsimonelli@gmail.com>
2021-06-23 18:16:27 -0500

    theme update app utility root

--- templates/javascript/src/App.js
+++ templates/javascript/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className='root wave-layer'>
+    <div className='root wave-layer min-h-screen flex justify-start items-center'>
       <h1>hello friend</h1>
     </div>
   ); // end return
